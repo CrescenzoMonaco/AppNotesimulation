0361edbad760d1825549edd7713175b771d6abcc
Crescenzo Monaco <crescenzomonaco.cm@gmail.com>
2023-11-21 23:05:16 +0100

    Initial Commit

---
+++ AppNotesimulation/AppNotesimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppNotesimulationApp.swift
+//  AppNotesimulation
+//
+//  Created by Crescenzo Monaco on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AppNotesimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
